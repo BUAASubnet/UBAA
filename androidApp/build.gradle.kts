7c12911c00e95c1dc881acef14c8a456d3481d06
MooreFoss <thelmh@outlook.com>
2026-03-24 17:14:36 +0800

    修复依赖

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
   alias(libs.plugins.androidApplication)
-  id("org.jetbrains.kotlin.android")
   alias(libs.plugins.composeMultiplatform)
   alias(libs.plugins.composeCompiler)
 }
