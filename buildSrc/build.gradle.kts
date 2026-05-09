773a1cd958b69c26d6d5304aee87551868e18cd7
MooreFoss <thelmh@outlook.com>
2026-05-09 20:10:29 +0800

    更新自动上传bhpan任务

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+  `kotlin-dsl`
+}
+
+repositories {
+  gradlePluginPortal()
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+  testImplementation(kotlin("test"))
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
